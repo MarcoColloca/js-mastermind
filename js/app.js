06cf8a86e77d3641e6ce705dcfde926c0977f89c
MarcoColloca <marco.colloca3511@gmail.com>
2024-03-15 17:54:31 +0100

    Refactor → cleaned up JS comments

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 console.log('JS Trial') 
 
-/// generare un array di 4 numeri univoci compresi da 1 a 9
+// 1 • generare un array di 4 numeri univoci compresi da 1 a 9
 
-/// leggere l'input inserito  (max 4 numeri) e trasformarlo in un array di 4 numeri interi
+// 2 • leggere l'input inserito  (max 4 numeri) e trasformarlo in un array di 4 numeri interi
 
-/// verificare se i valori inseriti corrispondono o sono presenti nell'array dei nuemri generati all'inizio
+// 3 • verificare se i valori inseriti corrispondono o sono presenti nell'array dei nuemri generati all'inizio
 
-/// rispondere con una stringa composta da o / x dove:
-///  • o = numero giusto posizione sbagliata
-///  • x = numero giusto posizione corretta
+// 4 • rispondere con una stringa composta da o / x dove:
+///         • o = numero giusto posizione sbagliata
+///         • x = numero giusto posizione corretta
 
 
 // funzione che crea una quantità di numeri univoci pari a "howMany" ognuno di essi copmreso tra "min" e "max".
@@ -103,10 +103,16 @@ const reset = document.getElementById('reset')
 const userNumbersDOMElement = document.getElementById('user-number')
 
 const resultBox = document.getElementById('result')
+
 const getNumbers = document.getElementById('submit-numbers')
 
+
+
+
 let gameNumbers = randomNumbers(1, 9, 4)
-console.log(gameNumbers)
+// console.log(gameNumbers) /* se vuoi barare, attiva il commento ed in console trovi i numeri :-) */
+
+
 
 
 getNumbers.addEventListener('click', playGame)
